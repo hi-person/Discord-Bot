770921f6222a7f8a39f2ff7516dd4a41cc831fb8
hi-person <94467261+hi-person@users.noreply.github.com>
2021-11-20 07:34:54 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import json
 import random
 from replit import db
-
+from keep_alive import keep_alive
 client = discord.Client()
 sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
 
@@ -91,5 +91,5 @@ async def on_message(message):
     else:
       db["responding"] = False
       await message.channel.send("Responding is off.")
-
+keep_alive()
 client.run(os.getenv('TOKEN'))
